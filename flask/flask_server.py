6021a9fa57c0f0b281c4431eff4e89ddf5c62b69
FireElementalNE <FireElemental.NE@gmail.com>
2021-02-21 10:38:08 -0500

    added styling

--- flask/flask_server.py
+++ flask/flask_server.py
@@ -5,28 +5,20 @@ from flask import Flask, jsonify, request, render_template
 from dice_sim_lib import play
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/game', methods=['POST'])
 def hello():
-
-    # POST request
     if request.method == 'POST':
-        print('Incoming..')
-        print(request.form['n1'])  # parse as JSON
         text = play(request.form['n1'], request.form['n2'], int(request.form['rounds']))
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(text)
-        # print(text)
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(text)
         return jsonify({'text':text})
 
-    # GET request
     else:
         message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers
+        return jsonify(message)
 
 @app.route('/')
 def index():
-    # render_template("script.js")
-    # look inside `templates` and serve `index.html`
     return render_template('index.html')
 
 if __name__ == '__main__':
--- flask/static/js/script.js
+++ flask/static/js/script.js
@@ -1,41 +1,44 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     $("#submitbutton").click(start_game);
-    var a;
 });
 
+function fade_in(t, id, key, final_line) {
+  if(!final_line) {
+    $('#output').append("<p id='" + id + "'></p>")  
+  } else {
+    $('#output').append("<p id='" + id + "' class='titles'></p>")  
+  }
+  $("#" + id).hide();
+  $("#" + id).html(t['text'][key].join('</br>'));
+  $("#" + id).fadeIn(1000);
+}
+
 function write_text(t) {
-  // $('#output').html(data['text'].join('</br>'));
-  console.log(Object.keys(t['text']).length);
-  console.log(t['text']);
-  var i, j;
+  var i;
   for (i = 1; i < Object.keys(t['text']).length; i++) {
     var id = "div" + i.toString();
-    console.log(t['text'][i.toString()]);
-    $('#output').append("<p id='" + id + "'></p>")
-    $("#" + id).hide();
-    // for(j = 0; j < t['text'][i.toString()].length; j++) {
-    $("#" + id).html(t['text'][i.toString()].join('</br>'));
-    $("#" + id).fadeIn(1000);
-    // }
-
+    fade_in(t, id, i.toString(), false);
   }
+  fade_in(t, 'result', 'result', true);
 }
+
 function start_game() {
-  var x, text;
+  
   n1 = $("#name1").val();
   n2 = $("#name2").val();
   rounds = $("#rounds").val();
+
   if (isNaN(rounds) || n1 == "" || n2 == "") {
     console.log("Invalid Input");
   } else {
+    
     $("#info_submission").hide();
     $("#names").html(n1 + " vs. " + n2 + " for " + rounds + " rounds. </br></br>");
     $("#gamespace").show();
+    
     var data = {"n1": n1, "n2": n2, "rounds": rounds};
-    console.log(data);
     $.ajax({
-        url: $SCRIPT_ROOT + "/hello",
+        url: $SCRIPT_ROOT + "/game",
         type: 'post',
         dataType: 'json',
         data: data,
